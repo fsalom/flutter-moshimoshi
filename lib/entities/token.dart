a2d9d2480ac5170bdfd8076c75ab52dfdcb82853
Fernando Salom <fer@rudo.es>
2024-02-13 12:50:09 +0100

    Fix token and tokens properties

--- lib/entities/token.dart
+++ lib/entities/token.dart
@@ -1,4 +1,16 @@
 class Token {
   String value = "";
   int timestampExpires = 0;
+
+  Token();
+
+  Token.fromJson(Map<String, dynamic> json)
+      : value = json['value'],
+        timestampExpires = json['timestampExpires'];
+
+  Map<String, dynamic> toJson() => {
+        'value': value,
+        'timestampExpires': timestampExpires,
+      };
+  
 }
--- lib/entities/tokens.dart
+++ lib/entities/tokens.dart
@@ -3,4 +3,9 @@ import "package:flutter_moshimoshi/entities/Token.dart";
 class Tokens {
   late Token acccesToken;
   late Token refreshToken;
+
+  init(Token accessToken, Token refreshToken) {
+    acccesToken = accessToken;
+    refreshToken = refreshToken;
+  }
 }
