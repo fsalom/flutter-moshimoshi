ef86015d06ecb3f296ce8a09869ee2ed6721c79a
Fernando Salom <fer@rudo.es>
2024-02-14 13:07:27 +0100

    Add main class

--- lib/flutter_moshimoshi.dart
+++ lib/flutter_moshimoshi.dart
@@ -1,7 +1,26 @@
 library flutter_moshimoshi;
+import 'dart:core';
+import 'dart:developer';
+import 'dart:js_util';
+import 'package:dio/dio.dart';
+import 'package:flutter_moshimoshi/authenticationCard/authentication_card_interface.dart';
+import 'package:flutter_moshimoshi/authenticator/authenticator_interface.dart';
+import 'package:flutter_moshimoshi/storage/storage_interface.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class MoshiMoshi {
+  var timeout = const Duration(seconds: 30);
+  late AuthenticatorInterface authenticator;
+  late StorageInterface storage;
+  late Dio dio;
+
+  init(AuthenticatorInterface authenticator, StorageInterface storage, Interceptor interceptor) {
+    this.authenticator = authenticator;
+    this.storage = storage;
+    dio = Dio(BaseOptions( connectTimeout: timeout));
+    dio.interceptors.add(interceptor);
+  }
+
+  load() {
+    
+  }
 }
