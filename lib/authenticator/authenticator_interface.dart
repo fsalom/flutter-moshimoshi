fee05531b8d6799ceda251b502263e13213826d6
Fernando Salom <fer@rudo.es>
2024-02-14 13:07:56 +0100

    Fix type to RequestOptions

--- lib/authenticator/authenticator_interface.dart
+++ lib/authenticator/authenticator_interface.dart
@@ -1,8 +1,9 @@
+import "package:dio/dio.dart";
 import "package:flutter_moshimoshi/storage/storage_interface.dart";
 
 abstract class AuthenticatorInterface {
     late StorageInterface tokenStore;
-    Future<Uri> authorize(Uri request);
+    Future<RequestOptions> authorize(RequestOptions request);
     Future<void> getNewToken(String parameters);
     Future<String> getCurrentToken();
     Future<bool> isLogged();
