4490ca6411241064c833797cdba0c77fe6eaf578
Fernando Salom <fer@rudo.es>
2024-02-14 13:07:16 +0100

    Add AuthInterceptor

---
+++ lib/interceptor/AuthInterceptor.dart
@@ -0,0 +1,35 @@
+
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_moshimoshi/authenticator/authenticator_interface.dart';
+
+class AuthInterceptors extends Interceptor {
+  late AuthenticatorInterface authenticator;
+
+  init(AuthenticatorInterface authenticator){
+    this.authenticator = authenticator;
+  }
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    // TODO: implement onRequest
+    super.onRequest(options, handler);
+    log(">>> DIO REQUEST PATH: ${options.path}");
+    log(">>> DIO REQUEST HEADERS: ${options.headers}");
+    authenticator.authorize(options);
+    return handler.next(options);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    // TODO: implement onResponse
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    // TODO: implement onError
+    super.onError(err, handler);
+  }
+}
