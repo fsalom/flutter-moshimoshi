778dd74890804c6a115ebe4b6b6e473c4e5c0eae
Fernando Salom <fer@rudo.es>
2024-02-17 22:50:17 +0100

    Fix authenticator interface

--- lib/authenticator/authenticator_interface.dart
+++ lib/authenticator/authenticator_interface.dart
@@ -1,11 +1,11 @@
 import "package:dio/dio.dart";
 import "package:flutter_moshimoshi/storage/storage_interface.dart";
 
-abstract class AuthenticatorInterface {
+abstract class AuthenticatorInterface {    
     late StorageInterface tokenStore;
     Future<RequestOptions> authorize(RequestOptions request);
     Future<void> getNewToken(String parameters);
-    Future<String> getCurrentToken();
+    Future<String?> getCurrentToken();
     Future<bool> isLogged();
     Future<void> logout();
 }
