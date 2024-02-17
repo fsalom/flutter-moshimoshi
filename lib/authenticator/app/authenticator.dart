8eb94e4de8b23cef86035dd70568f285fe18c77c
Fernando Salom <fer@rudo.es>
2024-02-17 22:47:53 +0100

    Add new functionality to authenticator implementation

--- lib/authenticator/app/authenticator.dart
+++ lib/authenticator/app/authenticator.dart
@@ -1,21 +1,24 @@
+import 'dart:ffi';
 import 'dart:isolate';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_moshimoshi/authenticationCard/authentication_card_interface.dart';
 import 'package:flutter_moshimoshi/authenticator/authenticator_interface.dart';
+import 'package:flutter_moshimoshi/entities/error.dart';
+import 'package:flutter_moshimoshi/entities/tokens.dart';
 import 'package:flutter_moshimoshi/storage/storage_interface.dart';
 import 'package:http/http.dart' as http;
 
 class Authenticator implements AuthenticatorInterface {
   @override
   late StorageInterface tokenStore;
+  late AuthenticatorCardInterface card;
 
-  init(StorageInterface tokenStore) {
-    this.tokenStore = tokenStore;
-  }
+  Authenticator(this.tokenStore, this.card);
   
   @override
   Future<RequestOptions> authorize(RequestOptions request) async {
-      var accessToken = Isolate.run<String>(() {    
+      var accessToken = Isolate.run<String?>(() {    
         return getCurrentToken();
       });
       request.headers["Authorization"] = "Bearer $accessToken";
@@ -23,9 +26,11 @@ class Authenticator implements AuthenticatorInterface {
   }
   
   @override
-  Future<String> getCurrentToken() {
-    // TODO: implement getCurrentToken
-    throw UnimplementedError();
+  Future<String?> getCurrentToken() async {
+    var tokens = await checkAccess().catchError((error) {
+      logout();
+      });
+    return tokens.acccesToken.value;
   }
   
   @override
@@ -35,15 +40,38 @@ class Authenticator implements AuthenticatorInterface {
   }
   
   @override
-  Future<bool> isLogged() {
-    // TODO: implement isLogged
-    throw UnimplementedError();
+  Future<bool> isLogged() async {
+    var tokens = await checkAccess();
+    return true;
   }
   
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> logout() async {
+    tokenStore.clear();
+    card.logout();
   }
 
+  Future<Tokens> checkAccess() async {
+    var accessToken = await tokenStore.getAccessToken()
+    .catchError((error) {
+         logout();
+      });
+    var refreshToken = await tokenStore.getRefreshToken()
+    .catchError((error) {
+         logout();
+      });
+    if (accessToken.isValid) {
+      return Tokens(accessToken, refreshToken);
+    } else {
+      if (refreshToken.isValid) {
+        var tokens = await card.refreshAccessToken(refreshToken.value);
+        tokenStore.setAccessToken(tokens.acccesToken);
+        tokenStore.setRefreshToken(tokens.refreshToken);    
+        return tokens;
+      } else {
+        await logout();
+        throw AuthorizationFailed();
+      }
+    } 
+  }
 }
