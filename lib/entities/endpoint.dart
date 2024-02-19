f13423fc09a3ec1f9482028ce9b5fc84d3665bf3
Fernando Salom <fer@rudo.es>
2024-02-19 17:26:30 +0100

    Refactor endpoint with http call included

--- lib/entities/endpoint.dart
+++ lib/entities/endpoint.dart
@@ -1,54 +1,30 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 enum Method { GET, POST, PUT, PATCH }
-
-class Parameter {
-  String key;
-  String value;
-
-  Parameter({required this.key, required this.value});
-}
 
 class Endpoint {
   late String url;
   late Method method;
-  List<Parameter> parameters = [];
-  List<Parameter> queryParams = [];
-  List<Parameter> formParams = [];
+  late Map<String, String> queryParams;
+  late Map<String, String> headers;
+  late Map<String, dynamic> formParams;
 
   Endpoint({this.url = "", this.method = Method.GET});
 
-  bool checkIfExists(List<Parameter> list, String key) {
-    for (var param in list) {
-      if (param.key == key) {
-        return true;
-      }
+  Future<Response> call() {
+    final url = Uri.parse(this.url); 
+    final body = jsonEncode(formParams);
+    switch (method) {
+      case Method.GET:
+        return http.get(url, headers: headers);
+      case Method.POST:
+        return http.post(url, headers: headers, body: body);    
+      case Method.PUT:
+        return http.post(url, headers: headers, body: body);    
+      case Method.PATCH:
+        return http.post(url, headers: headers, body: body);    
     }
-    return false;
-  }
-
-  void add(List<Parameter> formParams) {
-    formParams.forEach((param) {
-      if (!checkIfExists(this.formParams, param.key)) {
-        this.formParams = [...this.formParams, param];
-      } else {
-        this.formParams
-            .where((it) => it.key == param.key)
-            .forEach((it) => it.value = param.value);
-      }
-    });
-  }
-
-  http.Request getRequest() {
-    var request = http.Request(method.toString(), Uri.parse(url));
-
-    if (formParams.isNotEmpty) {
-      request.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-      request.body = formParams.map((param) {
-        return '${param.key}=${param.value}';
-      }).join('&');
-    }
-
-    return request;
   }
 }
