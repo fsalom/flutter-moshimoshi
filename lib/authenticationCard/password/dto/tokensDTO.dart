5ce661ec8feb6cc7e80192436d2d3d0a4cb5f55e
Fernando Salom <fer@rudo.es>
2024-02-24 16:02:48 +0100

    Update tokensDTO

--- lib/authenticationCard/password/dto/tokensDTO.dart
+++ lib/authenticationCard/password/dto/tokensDTO.dart
@@ -3,13 +3,13 @@ import 'package:json_annotation/json_annotation.dart';
 part 'tokensDTO.g.dart';
 
 @JsonSerializable()
-class TokenDTO {
+class TokensDTO {
   final String access_token;
   final String refresh_token;
   final int expires_in;
   int expirationTime;
 
-  TokenDTO(this.expirationTime, {required this.access_token, required this.refresh_token, required this.expires_in}) {
+  TokensDTO(this.expirationTime, {required this.access_token, required this.refresh_token, required this.expires_in}) {
     var expirationDate = DateTime.now().add(Duration(seconds: expires_in));
     expirationTime = expirationDate.microsecondsSinceEpoch;
   }
