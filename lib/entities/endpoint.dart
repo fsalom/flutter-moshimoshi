a871b4f2ff8d5b93f6a60e78129daeb2481c250b
Fernando Salom <fer@rudo.es>
2024-02-24 16:03:16 +0100

    Fix endpoint to add headers and correct enum

--- lib/entities/endpoint.dart
+++ lib/entities/endpoint.dart
@@ -2,29 +2,29 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
-enum Method { GET, POST, PUT, PATCH }
+enum Method { get, post, put, patch }
 
 class Endpoint {
   late String url;
   late Method method;
-  late Map<String, String> queryParams;
-  late Map<String, String> headers;
-  late Map<String, dynamic> formParams;
+  Map<String, String> headers = {};
+  Map<String, String> queryParams = {};
+  Map<String, dynamic> formParams = {};
 
-  Endpoint({this.url = "", this.method = Method.GET});
+  Endpoint({this.url = "", required this.headers, this.method = Method.get});
 
   Future<Response> call() {
     final url = Uri.parse(this.url); 
     final body = jsonEncode(formParams);
     switch (method) {
-      case Method.GET:
+      case Method.get:
         return http.get(url, headers: headers);
-      case Method.POST:
+      case Method.post:
         return http.post(url, headers: headers, body: body);    
-      case Method.PUT:
-        return http.post(url, headers: headers, body: body);    
-      case Method.PATCH:
-        return http.post(url, headers: headers, body: body);    
+      case Method.put:
+        return http.put(url, headers: headers, body: body);    
+      case Method.patch:
+        return http.patch(url, headers: headers, body: body);    
     }
   }
 }
