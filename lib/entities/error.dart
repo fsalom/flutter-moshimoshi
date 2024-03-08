c140eb4d6e874d41fa94c8029cb5c7156ccd5ac5
Fernando Salom <fer@rudo.es>
2024-03-08 21:54:33 +0100

    Add override

--- lib/entities/error.dart
+++ lib/entities/error.dart
@@ -1,7 +1,9 @@
 
 class AuthorizationFailed extends Error implements UnsupportedError {
+  @override
   final String? message;
   AuthorizationFailed([this.message]);
+  @override
   String toString() {
     var message = this.message;
     return (message != null)
