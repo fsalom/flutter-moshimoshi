15ffad19ab91507a08c9f3046d3f8b0db28884cd
Fernando Salom <fer@rudo.es>
2024-06-06 18:47:29 +0200

    Fix list of interceptors of type MoshiInterceptor

--- lib/flutter_moshimoshi.dart
+++ lib/flutter_moshimoshi.dart
@@ -2,13 +2,14 @@ library flutter_moshimoshi;
 import 'dart:core';
 import 'package:dio/dio.dart';
 import 'package:flutter_moshimoshi/authenticator/authenticator_interface.dart';
+import 'package:flutter_moshimoshi/entities/moshi_interceptor.dart';
 import 'package:flutter_moshimoshi/storage/storage_interface.dart';
 
 class MoshiMoshi {
   final _timeout = const Duration(seconds: 30);
   final AuthenticatorInterface authenticator;
   final StorageInterface storage;
-  final List<Interceptor> interceptors;
+  final List<MoshiInterceptor> interceptors;
   late Dio call;
   late Dio callAuthenticated;
 
@@ -20,8 +21,16 @@ class MoshiMoshi {
     call = Dio(BaseOptions(connectTimeout: _timeout));
     callAuthenticated = Dio(BaseOptions(connectTimeout: _timeout));
 
-    for (var interceptor in interceptors) {
-      callAuthenticated.interceptors.add(interceptor);
+    for (var moshiInterceptor in interceptors) {
+      switch (moshiInterceptor.type) {
+        case InterceptorType.nonAuthenticated:
+          callAuthenticated.interceptors.add(moshiInterceptor.interceptor);
+          break;
+        case InterceptorType.authenticated:
+          callAuthenticated.interceptors.add(moshiInterceptor.interceptor);
+          break;
+      }
+      
     }
   }
 }
